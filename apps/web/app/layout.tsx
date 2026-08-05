b55780020aacda493896387b4a00c14d6037e58a
HuyIT2706 <buivanhuy2706@gmail.com>
2026-08-05 14:59:39 +0700

    feat: add favicon, fontfamily in app

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,9 +1,19 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../assets/css/globals.css";
 
 export const metadata: Metadata = {
   title: "FitManagementPlatform",
   description: "FitManagementPlatform Application",
+  icons: {
+    icon: [
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
